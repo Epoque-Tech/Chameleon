9623a7251ae6695d00cc35e00a14807c5f71eb2c
User Jason <jason@voyager.jasonfavrod.com>
2015-05-30 09:09:25 +0000

    Removed Printing of HTML in BaseClassTest

--- vendor/epoque/chameleon/tests/BaseClassTest.php
+++ vendor/epoque/chameleon/tests/BaseClassTest.php
@@ -13,16 +13,7 @@ class BaseClassTest extends BaseClass {
 };
 
 
-print <<<HTML
-<!doctype html>
-<head>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-</head>
-<body>
-HTML;
-
 $bct = new BaseClassTest();
 $bct->pWarning('Warning! Something happened that may cause things to break.');
 $bct->pError('Doh! Something Broke.');
 
-print '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>';
