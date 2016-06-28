b67192bdd6c528b4ad9c4ab226c5b762cc34b5d5
Jason Favrod <jason@lakonacomputers.com>
2016-06-28 14:59:08 -0400

    Added GitHub Section

--- views/homepage.php
+++ views/homepage.php
@@ -1,4 +1,6 @@
-<?php use \Epoque\jQueryUI\Tabs; ?>
+<?php use Epoque\jQueryUI\Tabs; ?>
+<?php use Epoque\GitHub\Repos; ?>
+
 
 <div class="row">
     <div class="col-md-6">
@@ -10,15 +12,21 @@
                 'id' => 'news-tabs',
                 'class' => 'tabs',
                 'tabs' => [
-                    'econ-news' => 'Economy',
                     'biz-news' => 'Business',
                     'top-news' => 'Top Stories',
-                    'world-news' => 'World']
+                    'world-news' => 'World',
+                    'econ-news' => 'Economy']
                 ]);
             ?>
             <script src="<?php echo JS_DIR.'news.js'; ?>"></script>
         </section>
 
+        <br>
+        
+        <section id="github">
+            <script src="<?php echo JS_DIR.'github.js'; ?>"></script>
+        </section>
+        <?php //print_r(Repos::enumerate()); ?>
     </div>
 
     <div class="col-md-3 col-md-offset-1">
