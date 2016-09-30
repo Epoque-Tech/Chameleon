a6392733da5f61c6b599e8c0940642df452e9038
jason <jason@ubuntu-1604-lamp.starbase.lan>
2016-09-29 20:41:10 -1000

    Added Download Link to the Installer File

--- views/homepage.php
+++ views/homepage.php
@@ -12,11 +12,36 @@ print $parser->parse("
 
 Chameleon is an PHP framework for building Web applications. It requires Ubuntu
 Linux and Apache Web server. It also (somewhat) supports interactions with MySQL,
-Oracle DB, and SQLite. The framework is currently in the early beta phase of
-development and used in a few production applications. To find out how to use
-the framework checkout the [manual](/manual). To help with the development,
-report issues, or get in touch with the developer, see the github
+Oracle DB, and SQLite. The framework is currently in the pre-release
+development and should not be used in a production applications. To find out
+how to use the framework checkout the [manual](/manual). To help with the
+development, report issues, or get in touch with the developer, see the github
 [repo](https://github.com/not--p/Chameleon).
+
+## Getting Started ##
+
+Note prerequisites:
+
+    Ubuntu 16.04
+    git knowledge
+    sudo access
+
+### Download ###
+
+Download the installer:
+
+[https://owncloud.lakonacomputers.com/index.php/s/NPH3MreeahR396K](https://owncloud.lakonacomputers.com/index.php/s/NPH3MreeahR396K)
+
+### Installation ###
+
+Run the installer:
+
+    ./chameleon-install.bash
+
+Answer the prompts. They will ask for the project directory. Chameleon
+assumes the default Apache2 directory usage, and the project directory will be
+created in the /var/www directory -- virtual host configuration files are
+stored in /etc/apache2/site-enabled.
 ");
 ?>
     </div>
