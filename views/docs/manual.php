ce41c8580777106475115dfe19205effb00fdfc3
not--p <jason@lakonacomputers.com>
2016-10-18 20:13:34 +0000

    Editing Manual

--- views/docs/manual.php
+++ views/docs/manual.php
@@ -15,7 +15,6 @@ print $parser->parse("
     <li><a href=\"#gettingStarted\">Getting Started</a></li>
     <ol type=\"i\">
         <li><a href=\"#install\">Installation</a></li>
-        <li><a href=\"#setup\">Chameleon Setup</a></li>
     </ol>
     <li><a href=\"#conventions\">Conventions</a></li>
     <li><a href=\"#objects\">Chameleon Objects</a></li>
@@ -28,16 +27,19 @@ The Chameleon framework is used to quickly setup new Web applications. It
 currently only supports applications using Apache (2.6) webserver on Ubuntu
 16.04.
 
-
-
 ### <span id=\"install\">Installation</span>
 
-[https://owncloud.lakonacomputers.com/index.php/s/o0qEsigcjQIzXwU](https://owncloud.lakonacomputers.com/index.php/s/o0qEsigcjQIzXwU)
+Download the Chameleon installation script:
 
-### Running the Chameleon script <span id=\"setup\"></span>
+[https://owncloud.lakonacomputers.com/index.php/s/NPH3MreeahR396K](//owncloud.lakonacomputers.com/index.php/s/NPH3MreeahR396K)
+
+Run the installation script:
 
     sudo ./chameleon-install.bash
-    
+
+The script will prompt for the user the information needed to setup the new
+Chameleon project.
+
 ## Conventions <span id=\"conventions\"></span>
 
 ### Directory Structure
@@ -46,35 +48,16 @@ currently only supports applications using Apache (2.6) webserver on Ubuntu
 
 **log** → contains the log files.
 
-**resources** → contains `css`, database (`db`), `html`, image (`img`), javascript (`js`), and `php`.
+**resources** → contains CSS, database, HTML, images, JavaScript, and PHP.
 
 **vendor** → contains composer components.
 
 **views** → contains pages/scripts that are shown to the user.
 
-#### Notes on Directory Structure
-
--- The chameleon (bin/chameleon) script 
-
 ### DIR Constants
 All PHP constants defined in Chameleon that represent directories and
-are named 'such-and-such' `DIR` (i.e. `CSS_DIR`, `JS_DIR`, etc) end
+are named 'such-and-such' `_DIR` (i.e. `CSS_DIR`, `JS_DIR`, etc) end
 with a trailing slash.
-
-## Chameleon Script
-
-The chameleon script (located in the root directory of the application)
-runs with root permissions.
-
-The usage is as follows:
-
-    setup:         run this command to initialize a chameleon project. 
-    index:         restore/create the default index.php file. 
-    css:           restore/create the default custom.css file. 
-    config-php:    (re)configure project's PHP (config.php) 
-    config-js:     (re)configure project's JavaScript (resources/js/config.js) 
-    config-apache: (re)configure project's Apache configuration. 
-    logs:          (re)create project's log directory and log files.
 
 ## config.js
 
