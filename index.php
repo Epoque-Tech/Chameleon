448f4fc8093fea317a5616d5c98b6ae8a9bcba02
not--p <jason@lakonacomputers.com>
2017-05-07 01:44:55 +0000

    Removed Guts from index.php

--- index.php
+++ index.php
@@ -5,29 +5,6 @@ use Epoque\Chameleon\Presenter;
 
 
 HtmlHead::addGlobalCss(CSS_DIR.'custom.css');
-
-// Docs
-$docs = [
-    ['Manual' => VIEWS_DIR.'docs/manual.php']
-];
-
-// Tests
-$tests = [
-    [ 'CommonTest'   => VIEWS_DIR.'tests/commonTest.php'  ],
-    [ 'HtmlHeadTest' => VIEWS_DIR.'tests/htmlHeadTest.php'],
-    [ 'JSTest'       => VIEWS_DIR.'tests/JSTest.php'      ],
-    [ 'MySQLDBTest'  => VIEWS_DIR.'tests/MySQLDBTest.php' ]
-];
-
-
-foreach ($docs as $doc) {
-    Presenter::addRoute($doc);
-}
-
-foreach ($tests as $test) {
-    Presenter::addRoute($test);
-}
-
 ?>
 
 <!-- Dynamic HTML Template -->
